2b8d18014f67182367ddc0f805fe0d7f2099e8e2
Sabrina Nusrat Ayvi <sabrina.ayvi@selise.com.bd>
2020-01-27 19:08:23 +0600

    Asynchronous

--- JS/main.js
+++ JS/main.js
@@ -1,77 +1,23 @@
-// classes with  subclasses
+const  second = () =>{
 
-// var Person5 = function(name,  yearOfBirth,  Job){
-//     this.name = name;
-//     this.yearOfBirth = yearOfBirth;
-//     this.Job = Job;
-// }
+    setTimeout(()=>{
+        console.log(' Async hey there');
 
-// Person5.prototype.calculateAge = function(){
-//     var  age = new Date().getFullYear()-this.yearOfBirth;
-//     console.log(age);
-    
-// }
-
-// var  Athlets =  function(name,  yearOfBirth,  Job, olympicGames,  Madels){
-//     Person5.call(this,  name,  yearOfBirth, Job);
-//     this.olympicGames = olympicGames;
-//     this.Madels = Madels;
-
-// }
-
-
-
-
-// Athlets.prototype = Object.create(Person5.prototype);
-
-// var  johnAthlets5 = new Athlets('john', 1990,'swimmer',3,10);
-// Athlets.prototype.wonMedal = function(){
-//     this.Madels++;
-//     console.log(this.Madels);
-    
-// }
-
-// johnAthlets5.calculateAge();
-// johnAthlets5.wonMedal();
-
-
-
-class  Person6 {
-    constructor (name,  yearOfBirth, job){
-        this.name = name;
-        this.yearOfBirth = yearOfBirth;
-        this.job = job;
-    }
-
-    calculateAge(){
-        var  age = new Date().getFullYear() - this.yearOfBirth;
-        console.log(age);
-        
-    }
-}
-
-
-class  Athlets6 extends Person6{
-    constructor  (name ,yearOfBirth, job, olympicGames,  medals){
-        super(name ,yearOfBirth, job);
-
-        this.olympicGames = olympicGames;
-        this.medals = medals;
-
-
-    }
-
-    wonMedals(){
-        this.medals++;
-        console.log( this.medals);
-        
-    }
-
+    }, 2000);
     
 }
 
 
-const  johnAthlets6 = new Athlets6('john', 1990,'swimmer',3,10);
+const  first = () =>{
+    console.log('hey there');
+    second();
+    console.log('the end');
+    
+  
+}
 
-johnAthlets6.wonMedals();
-johnAthlets6.calculateAge();
+first();
+
+
+
+
