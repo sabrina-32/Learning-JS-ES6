2a0b4669c892763fe30eb793befafa5b02cf5c22
Sabrina Nusrat Ayvi <sabrina.ayvi@selise.com.bd>
2020-01-27 17:32:57 +0600

    classes

--- JS/main.js
+++ JS/main.js
@@ -1,39 +1,19 @@
-/// Maps
 
-const question = new Map();
-question.set('question', 'what is the  official  name  of the  latest  major  JS version?');
-
-question.set(1,'ES5');
-
-question.set(2,'ES6');
-question.set(3,'ES7');
-question.set(4,'ES8');
-
-question.set('correct', 3);
-
-question.set(true, 'Correct Answer');
+//ES6
 
 
-question.set(false, 'Wrong Answer, Please  try  Again!');
+class  Person6{
+    constructor (name,  yearOfBirth,  job){
+        this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+    }
 
 
-console.log(question.get('question'));
-
-console.log(question.size);
-
-if(question.has(4)){
-
-    console.log("ans 4 is here");   
-
+    calculateAge(){
+        var   age  = new Date().getFullYear - this.yearOfBirth;
+    console.log(age);
+    }
 }
-question.clear();
 
-
-
-question.forEach((value,  key)=> console.log(`this  is ${key}, and  it's  set  to ${value}`));
-
-
-for(let  [key,value  ] of question.entries()){
-
-
-}
+const john6= new Person6('John' , 1990, 'Teacher');
